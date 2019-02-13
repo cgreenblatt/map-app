ef7d5f3e14a5bea063c3f7afeb172373619d5174
Cynthia Greenblatt <cynthiagreenblatt@Cynthias-MacBook-Air.local>
2019-02-12 17:27:29 -0800

    fix: Change DetailsAddrPhoneWeb to function

--- src/DetailsAddrPhoneWeb.js
+++ src/DetailsAddrPhoneWeb.js
@@ -1,31 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const DetailsAddrPhoneWeb = (props) => {
 
-class DetailsAddrPhoneWeb extends React.Component {
+  let place = props.place
+  let addressArray = place.formatted_address.split(', ')
+  let website = place.details[0].website
 
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    let place = this.props.place
-    let addressArray = place.formatted_address.split(', ')
-    let website = place.details[0].website
-
-    return (
-      <section aria-label={`${place.name} address and phone number`} className='details-addr-phone' tabIndex='0'>
-        <h3>{addressArray[0]}</h3>
-        <h3>{addressArray[1]}, {addressArray[2]}</h3>
-        <h3 aria-label='phone'>{place.details[0].formatted_phone_number}</h3>
-        {website &&
-          <h3 aria-label='winery website' className='details-website' >
-            <a aria-label={place.name + "website"}href={website}>{place.name}</a>
-          </h3>
-        }
-      </section>
-    )
-  }
+  return (
+    <section aria-label={`${place.name} address and phone number`} className='details-addr-phone' tabIndex='0'>
+      <h3>{addressArray[0]}</h3>
+      <h3>{addressArray[1]}, {addressArray[2]}</h3>
+      <h3 aria-label='phone'>{place.details[0].formatted_phone_number}</h3>
+      {website &&
+        <h3 aria-label='winery website' className='details-website' >
+          <a aria-label={place.name + "website"}href={website}>{place.name}</a>
+        </h3>
+      }
+    </section>
+  )
 }
 
 DetailsAddrPhoneWeb.propTypes = {
